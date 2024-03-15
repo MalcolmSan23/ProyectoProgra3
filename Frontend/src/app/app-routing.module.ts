7e0fa3da9cbbf100327252346866093d7d1aff26
LuisCF <luiscfunes521@gmail.com>
2024-03-15 11:22:33 -0600

    configuracion para componentes

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmpleadosComponent} from './Components/empleados/empleados.component';
 
-const routes: Routes = [];
+const routes: Routes = [  {
+  path: 'empleados',
+  component :EmpleadosComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
