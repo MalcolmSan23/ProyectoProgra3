1ccc9c6e552aa97089a7682b1d035a3565f900cc
LuisCF <luiscfunes521@gmail.com>
2024-03-31 18:22:47 -0600

    configuracion de inicio de sesion

--- Frontend/src/app/Components/login/login.component.ts
+++ Frontend/src/app/Components/login/login.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../Services/auth.service';
+import { Router } from '@angular/router'; // Importa Router
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  errorMessage: string = ''; // Variable para almacenar mensajes de error
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService
+      .login({ username: this.username, password: this.password })
+      .subscribe(
+        (response) => {
+          console.log('Inicio de sesión exitoso:', response);
+          // Aquí podrías redirigir al usuario a otra página de la aplicación
+          this.router.navigateByUrl('/navegacion');
+        },
+        (error) => {
+          console.error('Error al iniciar sesión:', error);
+          this.errorMessage =
+            'Credenciales incorrectas. Por favor, inténtalo de nuevo.'; // Mensaje de error para credenciales incorrectas
+          // También podrías manejar otros tipos de errores aquí, como problemas de conexión
+        }
+      );
+  }
 }
--- Frontend/src/app/guards/auth.guard.ts
+++ Frontend/src/app/guards/auth.guard.ts
@@ -1,5 +1,19 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../Services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (this.authService.isLoggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']); // Redirigir al usuario a la página de inicio de sesión si no está autenticado
+      return false;
+    }
+  }
+}
