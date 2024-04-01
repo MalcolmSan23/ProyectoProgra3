beb322161224562e32de8be781d8c4636c240ae1
LuisCF <luiscfunes521@gmail.com>
2024-03-31 18:53:57 -0600

    login y log out en el nav

--- Frontend/src/app/Components/navegacion/navegacion.component.ts
+++ Frontend/src/app/Components/navegacion/navegacion.component.ts
@@ -1,16 +1,19 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../Services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navegacion',
   templateUrl: './navegacion.component.html',
-  styleUrls: ['./navegacion.component.css']
+  styleUrls: ['./navegacion.component.css'],
 })
 export class NavegacionComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  logout(): void {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
   }
 
+  ngOnInit(): void {}
 }
-
--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmpleadosComponent } from './Components/empleados/empleados.component';
 import { EmpleadosEditComponent } from './Components/empleados-edit/empleados-edit.component';
 import { EspecialidadComponent } from './Components/especialidad/especialidad.component';
-import { EspecialidadEditComponent} from './Components/especialidad-edit/especialidad-edit.component';
+import { EspecialidadEditComponent } from './Components/especialidad-edit/especialidad-edit.component';
 import { NivelEduComponent } from './Components/niveledu/niveledu.component';
 import { NivelEduEditComponent } from './Components/niveledu-edit/niveledu-edit.component';
 import { CaparecibidaComponent } from './Components/caparecibida/caparecibida.component';
@@ -18,86 +18,111 @@ import { TiponiveleduComponent } from './Components/tiponiveledu/tiponiveledu.co
 import { TiponiveleduEditComponent } from './Components/tiponiveledu-edit/tiponiveledu-edit.component';
 import { TituloComponent } from './Components/titulo/titulo.component';
 import { TituloEditComponent } from './Components/titulo-edit/titulo-edit.component';
-import { NavegacionComponent} from './Components/navegacion/navegacion.component';
+import { NavegacionComponent } from './Components/navegacion/navegacion.component';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginComponent } from './Components/login/login.component';
 
 const routes: Routes = [
   {
+    path: 'login',
+    component: LoginComponent,
+  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
     path: 'empleados',
     component: EmpleadosComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'empleados/edit/:id',
-    component :EmpleadosEditComponent
+    component: EmpleadosEditComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'especialidad',
     component: EspecialidadComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'especialidad/edit/:id',
-    component :EspecialidadEditComponent
+    component: EspecialidadEditComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'niveledu',
     component: NivelEduComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'niveledu/edit/:id',
-    component :NivelEduEditComponent,
+    component: NivelEduEditComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'caparecibida',
     component: CaparecibidaComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'caparecibida/edit/:id',
-    component :CaparecibidaEditComponent,
+    component: CaparecibidaEditComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'facultad',
     component: FacultadComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'facultad/edit/:id',
-    component :FacultadEditComponent,
+    component: FacultadEditComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'nomcentro',
     component: NomcentroComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'nomcentro/edit/:id',
-    component : NomcentroEditComponent,
+    component: NomcentroEditComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'tipocapacitacion',
     component: TipocapacitacionComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'tipocapacitacion/edit/:id',
-    component : TipocapacitacionEditComponent,
+    component: TipocapacitacionEditComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'tiponiveledu',
     component: TiponiveleduComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'tiponiveledu/edit/:id',
-    component : TiponiveleduEditComponent,
+    component: TiponiveleduEditComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'titulo',
     component: TituloComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'titulo/edit/:id',
-    component : TituloEditComponent,
+    component: TituloEditComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'navegacion',
-    component:NavegacionComponent
+    component: NavegacionComponent,
+    canActivate: [AuthGuard],
   },
-
 ];
 
 @NgModule({
